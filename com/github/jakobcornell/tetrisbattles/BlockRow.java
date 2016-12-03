deb3188758bc9ec4461703f04e6d851d92eac183
Jakob Cornell <jakobcornell@gmail.com>
2016-12-03 15:43:05 -0500

    Implement Block colors, Game features

--- com/github/jakobcornell/tetrisbattles/BlockRow.java
+++ com/github/jakobcornell/tetrisbattles/BlockRow.java
@@ -1,11 +1,15 @@
 package com.github.jakobcornell.tetrisbattles;
 
-public class BoardRow {
+public class BlockRow {
 	private Block[] blocks;
 	private boolean full;
 
-	public BoardRow() {
+	public BlockRow() {
 		
+	}
+
+	public Block get(int column) {
+		return blocks[column];
 	}
 
 	public boolean isFull() {
--- com/github/jakobcornell/tetrisbattles/Game.java
+++ com/github/jakobcornell/tetrisbattles/Game.java
@@ -1,18 +1,35 @@
 package com.github.jakobcornell.tetrisbattles;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class Game {
+	public static final int width = 12, height = 30;
+	
 	// direction blocks move
 	public static enum Direction {
 		UP, DOWN
 	}
 
 	private BoardRow[] rows;
+	private Set<Tetromino> activeTets;
 
 	public Game() {
-		
+		rows = new BoardRow[height];
+		for (int i = 0; i < rows.length; i += 1) {
+			rows[i] = new BoardRow();
+		}
 	}
 
 	public void tick() {
 		
+	}
+
+	public void getBlocks(Block[][] blocks) {
+		for (int i = 0; i < height; i += 1) {
+			for (int j = 0; j < width; j += 1) {
+				blocks[i][j] rows[i].get(j);
+			}
+		}
 	}
 }
