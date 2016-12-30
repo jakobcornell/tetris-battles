29e2ae1ebcf20a2dd0b5fa3cf5a3a3e9e72514a3
Jakob Cornell <jakobcornell@gmail.com>
2016-12-30 13:28:30 -0600

    Var-size `Tetromino` changes

--- com/github/jakobcornell/tetrisbattles/Tetromino.java
+++ com/github/jakobcornell/tetrisbattles/Tetromino.java
@@ -5,39 +5,22 @@ import java.util.Random;
 
 public class Tetromino {
 	public Block[][] blocks;
-	public final RotationMode rotationMode;
 	public boolean isFrozen = false;
-	public int age = 0; // increment each frame: trigger default movement on specific value
+	public int age = 0;
 	public int row, col;
 	protected static Random random = new Random();
 
-	protected Tetromino(Block[][] blocks, RotationMode rotationMode) {
-		this.blocks = blocks;
-		this.rotationMode = rotationMode;
-	}
+	public Tetromino(Game.Color color) {
+		TetrominoPrototype proto = TetrominoPrototype.getRandom(random);
 
-	public Tetromino(TetrominoPrototype proto, Game.Direction movement) {
-		this(new Block[4][4], proto.rotationMode);
-		int bit = 0, step = 0;
-		if (movement == Game.Direction.DOWN) {
-			bit = 0; step = 1;
-		} else if (movement == Game.Direction.UP) {
-			bit = 15; step = 15;
-		} else {
-			throw new IllegalArgumentException("movement must be UP or DOWN");
-		}
-		for (Block[] blockRow : blocks) {
-			for (int c = 0; c < 4; c += 1) {
-				if ((1 << bit & proto.template) != 0) {
-					blockRow[c] = new Block();
-				}
-				bit = (bit + step) & 0xF;
+		int dim = proto.dimension;
+		blocks = new Block[dim][dim];
+		for (int i = 0; i < dim * dim; i += 1) {
+			if (proto.template >> (dim * dim - 1 - i) & 1 != 0) {
+				blocks[i / dim][i % dim] = new Block();
 			}
 		}
-	}
 
-	public Tetromino(Game.Direction movement, Game.Color color) {
-		this(TetrominoPrototype.getRandom(random), movement);
 		float ang = (float) Math.PI * (random.nextFloat() * 14 + 1) / 32;
 		float h = (color == Game.Color.RED) ? 0f : 0.5f;
 		float s = (float) Math.cos(ang);
@@ -56,18 +39,18 @@ public class Tetromino {
 	}
 
 	protected static enum TetrominoPrototype {
-		I(0x0F00, RotationMode.EVEN),
-		O(0x0660, RotationMode.EVEN),
-		T(0x0720, RotationMode.ODD),
-		S(0x0360, RotationMode.ODD),
-		Z(0x0630, RotationMode.ODD),
-		J(0x0710, RotationMode.ODD),
-		L(0x0740, RotationMode.ODD);
+		I(0x0F00, 4),
+		J(0470, 3),
+		L(0170, 3),
+		O(0b1111, 2),
+		S(0360, 3),
+		T(0270, 3),
+		Z(0630, 3);
 
 		private final int template;
-		private final RotationMode rotationMode;
+		private final int dimension;
 
-		private TetrominoPrototype(int template, RotationMode rotationMode) {
+		private TetrominoPrototype(int template, int dimension) {
 			this.template = template;
 			this.rotationMode = rotationMode;
 		}
@@ -77,9 +60,5 @@ public class Tetromino {
 		public static TetrominoPrototype getRandom(Random random) {
 			return values[random.nextInt(values.length)];
 		}
-	}
-
-	public static enum RotationMode {
-		EVEN, ODD;
 	}
 }
